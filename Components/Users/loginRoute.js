75578bc2196abb55a6ca7bf83def5108814b4a25
aliosman21 <amrali975@gmail.com>
2021-01-06 20:25:01 +0200

    connected successfully to DB from another file and set up the database

--- Components/Users/loginRoute.js
+++ Components/Users/loginRoute.js
@@ -1,7 +1,13 @@
 const router = require("express").Router();
+const jwt = require("jsonwebtoken");
+const model = require("./DBdealer");
 
 router.post("/login", async (req, res) => {
    //will check the database and send back true or false with jwt
+
+   console.log(req.body.email);
+   model.checkCredentials(req.body.email, req.body.password);
+
    res.send({ title: "GOOD" });
 });
 
